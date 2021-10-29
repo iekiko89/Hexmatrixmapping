a5b085a277ce99bbad17f88756f9b37cc9d4196d
iekiko89 <59753813+iekiko89@users.noreply.github.com>
2021-10-29 01:03:47 -0500

    another step

--- src/hextile.cpp
+++ src/hextile.cpp
@@ -3,8 +3,8 @@
 
 Hexagon::Hexagon(int q=0, int r=0, int hex_Number=1){
     //definining the first led of the hexagon and the last led of the hexagon
-    static int hex_Start_Led=(1-1)*leds_per_hexagon;
-    static int hex_End_Led=(1)*leds_per_hexagon;
+    static int hex_Start_Led=(hex_Number-1)*leds_per_hexagon;
+    static int hex_End_Led=(hex_Number)*leds_per_hexagon;
 }
 //creating a class function to a hexagon
 void Hexagon::fill_hexagon(struct CRGB * leds, const struct CRGB& color){
